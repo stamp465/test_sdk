0d84c8f4bf003060d0dcd377874bc97903535bb9
Witnapat Champaruang <witnapat.cha@ascendcorp.com>
2024-04-25 11:12:00 +0700

    Webview done

--- StampLib/src/main/java/com/example/stamplib/StampLib.kt
+++ StampLib/src/main/java/com/example/stamplib/StampLib.kt
@@ -3,6 +3,8 @@ package com.example.stamplib
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.appcompat.app.AppCompatActivity
 
 object StampLib {
@@ -13,8 +15,27 @@ object StampLib {
 }
 
 class StampActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_stamp)
+
+        // Find the WebView by its id
+        webView = findViewById(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+
+        // Load a web page
+        val url = "https://www.google.com/"
+        webView.loadUrl(url)
+    }
+
+    override fun onBackPressed() {
+        if (webView.canGoBack())
+            webView.goBack()
+        else
+            super.onBackPressed()
     }
 }
